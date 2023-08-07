3c9b97063c9accf194b16b3853869fb91358e648
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-08-07 12:26:17 +0200

    numbers in python

---
+++ 02_Numbers.py
@@ -0,0 +1,40 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+
+#................................ Python Numbers...................
+# There are three numeric types in Python:
+
+# int
+x=1
+# float
+y = 2.6
+# complex
+z = 1j
+
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+print(w, type(w))
