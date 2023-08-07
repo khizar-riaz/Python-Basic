b9e316e049addaa0821c4df3feb94851f782a39a
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-08-07 12:06:54 +0200

    veriables and comment in python

---
+++ 01_Hello.py
@@ -0,0 +1,35 @@
+print ("Hello World")
+
+#this is commment in python
+
+#This is a comment
+#written in
+#more than just one line
+
+#..........................variables.............................
+
+x = 5
+y = "khizar"
+print(x)
+print(y)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
