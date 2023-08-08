634691fb303aab8f4b4d4abc8810c511a2e142db
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-08-08 21:03:41 +0200

    string in python

---
+++ 03_String.py
@@ -0,0 +1,31 @@
+str = "hello"
+print(str)
+
+a = """Lorem ipsum dolor sit amet aliqua."""
+print(a)
+
+
+#Slicing 
+b = "Hello world!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+#Modify
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+#String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#String Format
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
